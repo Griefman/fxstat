9dfb9ec460ab04a426f1315ad9280d3ac2c2c3ec
Griefman <stanislavparshin@yandex.ru>
2022-12-30 17:50:06 +0100

    folder stats added

--- prado/prado_multy.py
+++ prado/prado_multy.py
@@ -1,11 +1,11 @@
 import openpyxl
 
-wb = openpyxl.load_workbook('D:\GitHub\/fxstat\prado\jappu_report-28122022.xlsx')
+wb = openpyxl.load_workbook('D:\GitHub\/fxstat\prado\prado_multy_trades.xlsx')
 sheet = wb.get_sheet_by_name('Лист1')
 
 print(sheet)
 reports = []
-for cellObj in sheet['A1': 'N690']:
+for cellObj in sheet['A1': 'N1693']:
     report = []
     for cell in cellObj:
         report.append(cell.value)
@@ -14,7 +14,6 @@ for cellObj in sheet['A1': 'N690']:
 
 print(reports[0])
 
-# report_dct = {}
 
 pairs_dct = {}
 count = 0
---
+++ stats/all_accounts.py
@@ -0,0 +1,31 @@
+# Waka + FW + Rringe
+icm5_685_chf = 10811
+
+# Prado
+btc = 16525
+robo4_233 = 0.228 * btc
+robo4_297 = 3180
+jappu4_306 = 413
+# TradeAngel
+icm4_451_usd = 7036
+robo4_612_eur = 6463
+
+# WolfScalper
+rann5_353_usdt = 901
+
+# Signal + WolfScalper
+icm5_679_usd = 1103
+
+# Other
+robo4_109 = 408
+aafx5_665 = 250
+
+all_accounts = [icm5_685_chf, robo4_233, robo4_297, jappu4_306, icm4_451_usd, robo4_612_eur, rann5_353_usdt, icm5_679_usd, robo4_109, aafx5_665]
+
+count_accounts = 0
+for acc in all_accounts:
+  count_accounts += acc
+
+print(count_accounts)
+
+
