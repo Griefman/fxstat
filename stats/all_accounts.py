80084a96672f18810aa91003cbe1136d558a5015
Griefman <stanislavparshin@yandex.ru>
2023-01-08 04:11:32 +0100

    all_accouts changed

--- stats/all_accounts.py
+++ stats/all_accounts.py
@@ -1,17 +1,17 @@
-eurusd = 1.068
-usdchf = 0.925
-btc = 16525
+eurusd = 1.051
+usdchf = 0.938
+btc = 16925
 
 # Waka + FW + Fringe
 icm5_685_chf = 10811 / usdchf
 
 # Prado
-robo4_233 = 0.228 * btc
-robo4_297 = 3180
-jappu4_306 = 413
+robo4_233_btc = 0.233 * btc
+robo4_297_usd = 3268
+jappu4_306 = 436
 # TradeAngel
-icm4_451_usd = 7036
-robo4_612_eur = 6463 * eurusd
+icm4_451_usd = 7242
+robo4_612_eur = 6562 * eurusd
 
 # WolfScalper
 rann5_353_usdt = 901
@@ -20,14 +20,20 @@ rann5_353_usdt = 901
 icm5_679_usd = 1103
 
 # Other
-robo4_109 = 408
+robo4_109_usd = 408
 aafx5_665 = 250
 
-all_accounts = [icm5_685_chf, robo4_233, robo4_297, jappu4_306, icm4_451_usd, robo4_612_eur, rann5_353_usdt,
-                icm5_679_usd, robo4_109, aafx5_665]
+all_accounts = [icm5_685_chf, robo4_233_btc, robo4_297_usd, jappu4_306, icm4_451_usd, robo4_612_eur, rann5_353_usdt,
+                icm5_679_usd, robo4_109_usd, aafx5_665]
+
+robo_accounts = sum([robo4_233_btc, robo4_297_usd, robo4_612_eur, robo4_109_usd])
+icm_accounts = sum([icm5_685_chf, icm4_451_usd, icm5_679_usd])
 
 count_accounts = 0
 for acc in all_accounts:
   count_accounts += acc
 
-print(count_accounts)
+print(f'Сумма всех счетов: {round(count_accounts, 2)} $')
+print(f'Roboforex: {round(robo_accounts, 2)}')
+print(f'IC Markets: {round(icm_accounts, 2)}')
+
