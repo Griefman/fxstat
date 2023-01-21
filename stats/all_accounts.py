6841e663635ef3a900a2e22c0c95458d52baf3be
Griefman <stanislavparshin@yandex.ru>
2023-01-21 01:51:33 +0100

    new data added in all_accounts

--- stats/all_accounts.py
+++ stats/all_accounts.py
@@ -1,27 +1,29 @@
-eurusd = 1.051
-usdchf = 0.938
-btc = 16925
+eurusd = 1.085
+usdchf = 0.92
+btc = 22648
 
 # Waka + FW + Fringe
-icm5_685_chf = 10811 / usdchf
+icm5_685_chf = 11511 / usdchf
 
 # Prado
-robo4_233_btc = 0.233 * btc
-robo4_297_usd = 3268
-jappu4_306 = 436
+robo4_233_btc = 0.258 * btc
+robo4_297_usd = 2778
+jappu4_306 = 388
+prado = robo4_233_btc + robo4_297_usd + jappu4_306
+print(f'Prado: {int(prado)} $')
 # TradeAngel
-icm4_451_usd = 7242
-robo4_612_eur = 6562 * eurusd
+icm4_451_usd = 6603
+robo4_612_eur = 6249 * eurusd
 
-# WolfScalper
-rann5_353_usdt = 901
+# Among Us
+rann5_353_usdt = 960
 
 # Signal + WolfScalper
-icm5_679_usd = 1103
+icm5_679_usd = 899
 
 # Other
 robo4_109_usd = 408
-aafx5_665 = 250
+aafx5_665 = 259
 
 all_accounts = [icm5_685_chf, robo4_233_btc, robo4_297_usd, jappu4_306, icm4_451_usd, robo4_612_eur, rann5_353_usdt,
                 icm5_679_usd, robo4_109_usd, aafx5_665]
