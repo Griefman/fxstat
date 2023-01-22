300ca462132edb55fd1c08c10fd8663d354998e2
Griefman <stanislavparshin@yandex.ru>
2023-01-22 23:41:59 +0100

    o-account changed

--- stats/o-accounts.py
+++ stats/o-accounts.py
@@ -1,16 +1,16 @@
-eurusd = 1.068
+eurusd = 1.08
 
 # Waka
-icm5_134_usd = 5272
+icm5_134_usd = 5200
 
 # FW
-icm5_120_usd = 5075
+icm5_120_usd = 5403
 
 # Prado
-icm5_942_eur = 2718 * eurusd
+icm5_942_eur = 2864 * eurusd
 
 # TradeAngel
-icm4_831_usd = 6500
+icm4_831_usd = 6171
 
 o_accounts = [icm5_134_usd, icm5_120_usd, icm5_942_eur, icm4_831_usd]
 count_accounts = 0
@@ -18,4 +18,5 @@ for acc in o_accounts:
   count_accounts += acc
 
 print(count_accounts)
-
+# Недельные данные на 22.01.2023
+weeks = [19749, 19867]
