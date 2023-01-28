32378226949c45c13edf930e335be0a79209f54b
Griefman <stanislavparshin@yandex.ru>
2023-01-28 03:57:40 +0300

    ta_probality changed

--- tradeangel/min_semples.py
+++ tradeangel/min_semples.py
@@ -1,3 +1,22 @@
+import numpy as np
+
 def coin_flip(p):
   result = np.random.binomial(1, p)
   return result
+
+count_0 = 0
+count_1 = 0
+
+for i in range(10000):
+  semple = coin_flip(0.5)
+  if semple == 0:
+    count_0 += 1
+  else:
+    count_1 += 1
+
+percent_0 = count_0 / (count_0 + count_1)
+percent_1 = count_1 / (count_0 + count_1)
+
+
+print(f'{round(percent_0, 5)} %')
+print(f'{round(percent_1, 5)} %')
