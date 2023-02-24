db5fcbb60cb070155425dd69f86c724bc8d6fd35
Griefman <stanislavparshin@yandex.ru>
2023-02-25 01:50:20 +0300

    =====

--- prado/prado_multy.py
+++ prado/prado_multy.py
@@ -1,28 +1,29 @@
 import openpyxl
 
-wb = openpyxl.load_workbook('D:\GitHub\/fxstat\prado\prado_multy_trades.xlsx')
+wb = openpyxl.load_workbook('Y:\YandexDisk\GitHub\/fxstat\prado\prado_multy_trades_do-28022024.xlsx')
 sheet = wb.get_sheet_by_name('Лист1')
 
 print(sheet)
 reports = []
-for cellObj in sheet['A1': 'N1693']:
+for cellObj in sheet['A3': 'I2279']:
     report = []
     for cell in cellObj:
         report.append(cell.value)
-    if 'cancelled' not in report:
+    if 'cancelled' not in report and ('usdjpy' in report or 'cadjpy' in report):
         reports.append(report)
 
-print(reports[0])
-
+# print(reports[:-10])
+print(len(reports))
 
 pairs_dct = {}
 count = 0
 for report in reports:
-    count += report[-1]
-    if report[4] in pairs_dct:
-        pairs_dct[report[4]] += report[-1] + float(report[-2]) + float(report[-3]) + float(report[-4])
+    profit = float(report[-1]) + float(report[-2]) + float(report[-3]) + float(report[-4])
+    count += profit
+    if report[2] in pairs_dct:
+        pairs_dct[report[2]] += profit
     else:
-        pairs_dct[report[4]] = report[-1] + float(report[-2]) + float(report[-3]) + float(report[-4])
+        pairs_dct[report[2]] = profit
 
 print(count)
 for k, v in pairs_dct.items():
