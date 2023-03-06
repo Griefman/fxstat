c6dfb825b0fe3ae0e820a56d62c167bf1cffe9c6
Griefman <stanislavparshin@yandex.ru>
2023-03-06 22:18:40 +0100

    functions added in jappu.py

--- prado/Jappu/jappu_dds.py
+++ prado/Jappu/jappu_dds.py
@@ -10,10 +10,19 @@ df = pd.read_csv(file, sep=';')
 # df_dds = pd.Series(df['Equity'] - df['Balance'], name='dds')
 # print(f'Максимальная просадка: {int(df_dds.min()) - 1}$')
 
+
+
+def dataframe_by_month(month, dataframe):
+    dataframe_month = dataframe.loc[dataframe['Date'].str.contains(month)]
+    return dataframe_month
+
+def get_max_dd_by_month(month_dataframe):
+    print(month_dataframe.loc[month_dataframe['dd'] == month_dataframe['dd'].min()])
+
 df['dd'] = df['Equity'] - df['Balance']
 # print(df)
+dates = ['2023.02', '2023.03']
+for date in dates:
+    month_df = dataframe_by_month(date, df)
+    get_max_dd_by_month(month_df)
 
-big_dds = df['dd'].min()
-print(big_dds)  
-xxx = df.loc[df['dd'] == df['dd'].min()]
-print(xxx)
