ae0cb321cbcaa1c052a57120449b106cb62850e7
Griefman <stanislavparshin@yandex.ru>
2023-03-07 21:08:22 +0100

    path to file for VScode PowerVPD added

--- prado/Jappu/jappu_dds.py
+++ prado/Jappu/jappu_dds.py
@@ -1,13 +1,11 @@
-# import os
-# print("PYTHONPATH:", os.environ.get('PYTHONPATH'))
-# print("PATH:", os.environ.get('PATH'))
-
-
 import pandas as pd
 
 # file = 'C:\Users\Administrator\AppData\Roaming\MetaQuotes\Terminal\/6E0D8AD1CB44552398DC2AD0D2C56F9E\MQL4\Files\/142054306_1.csv'
 
-file = '142054306_1.csv'
+file = 'prado\\Jappu\\142054306_1.csv'
+# file = '142054306_1.csv' # for Pycharm
+
+
 df = pd.read_csv(file, sep=';')
 
 
