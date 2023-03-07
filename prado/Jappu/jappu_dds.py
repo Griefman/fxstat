f0035a526ed71898cf883fec46ae4cc5dd844d23
Griefman <stanislavparshin@yandex.ru>
2023-03-07 23:18:57 +0300

    file refacoring

--- prado/Jappu/jappu_dds.py
+++ prado/Jappu/jappu_dds.py
@@ -6,9 +6,6 @@ file = 'prado\\Jappu\\142054306_1.csv'
 
 df = pd.read_csv(file, sep=';')
 
-
-# df_dds = pd.Series(df['Equity'] - df['Balance'], name='dds')
-# print(f'Максимальная просадка: {int(df_dds.min()) - 1}$')
 def dataframe_by_month(month, dataframe):
     dataframe_month = dataframe.loc[dataframe['Date'].str.contains(month)]
     return dataframe_month
@@ -17,8 +14,9 @@ def dataframe_by_month(month, dataframe):
 def get_max_dd_by_month(month_dataframe):
     print(month_dataframe.loc[month_dataframe['dd'] == month_dataframe['dd'].min()])
 
+
 df['dd'] = df['Equity'] - df['Balance']
-# print(df)
+
 dates = ['2023.02', '2023.03']
 for date in dates:
     month_df = dataframe_by_month(date, df)
