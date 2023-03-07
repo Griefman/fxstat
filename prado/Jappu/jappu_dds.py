056fc8e8b774e63ed5fd36649df055bba4c8cc7e
Griefman <stanislavparshin@yandex.ru>
2023-03-07 23:18:05 +0300

    file refactoring

--- prado/Jappu/jappu_dds.py
+++ prado/Jappu/jappu_dds.py
@@ -1,7 +1,5 @@
 import pandas as pd
 
-# file = 'C:\Users\Administrator\AppData\Roaming\MetaQuotes\Terminal\/6E0D8AD1CB44552398DC2AD0D2C56F9E\MQL4\Files\/142054306_1.csv'
-
 file = 'prado\\Jappu\\142054306_1.csv'
 # file = '142054306_1.csv' # for Pycharm
 
